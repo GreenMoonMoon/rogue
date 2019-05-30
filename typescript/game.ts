dedb82e357c1b30ce6171561183c2db282180978
Josue Boisvert <josue.boisvert@reelfx.com>
2019-05-30 15:42:53 -0400

    add rendering function for object in viewport

--- typescript/game.ts
+++ typescript/game.ts
@@ -1,35 +1,103 @@
 import { point } from "./utils";
 
-let player: GameObject;
 let gameObjects: GameObject[];
+let player: GameObject;
+let map: number[][];
+let needUpdate = false;
 
 class GameObject {
+    name: string;
     tileID: number;
-    coordinate: point
-    constructor(tileID: number) {
-        this.tileID = tileID
-        this.coordinate = { x: 0, y: 0 }
+    coordinate: point;
+    constructor(name: string, tileID: number) {
+        this.name = name;
+        this.tileID = tileID;
+        this.coordinate = { x: 0, y: 0 };
+    }
+
+    move(x: number, y: number) {
+        this.coordinate.x += x;
+        this.coordinate.y += y;
     }
 }
 
 export class Game {
-    constructor() { }
-
-    runSteps() {
-
+    constructor() {
+        gameObjects = [];
+        player = new GameObject("hero", 27);
+        gameObjects.push(player);
+        initMap();
     }
 
     mapRect(x: number, y: number, w: number, h: number): number[][] {
-        return [];
+        let m: number[][] = [];
+        for(let i = 0; i < h; i++){
+            m[i] = [];
+            for(let j = 0; j < w; j++){
+                m[i][j] = map[i + y][j + x];
+            }
+        }
+        for (let go of gameObjects) {
+            m[go.coordinate.x - x][go.coordinate.y - y] = go.tileID;
+        }
+        return m;
     }
 
-    udpateCharacter(tileID?: number){
-        if(tileID){
-            player.tileID = tileID
+    update() { }
+
+    forceUpdate(){
+        needUpdate = true;
+    }
+
+    loop(){
+        window.requestAnimationFrame(() => this.loop())
+        if (needUpdate){
+            this.update();
+            needUpdate = false;
         }
+    }
+
+    addObject(gameObject: {}){
+        // Is this acceptable? I doupt it. Maybe I should expose the GameObject class.
+        gameObjects.push(<GameObject>gameObject);
     }
 }
 
 export class Controller {
-    constructor() { }
+    constructor(keymap?: {}) {
+        window.addEventListener("keydown", (event: KeyboardEvent) => this.handleKeyDown(event));
+    }
+
+    handleKeyDown(event: KeyboardEvent) {
+        switch (event.keyCode) {
+            case 37:
+                player.move(0, -1);
+                break;
+            case 38:
+                player.move(-1, 0);
+                break;
+            case 39:
+                player.move(0, 1);
+                break;
+            case 40:
+                player.move(1, 0);
+                break; ``
+        }
+        needUpdate = true;
+    }
+}
+
+function initMap() {
+    map = [
+        [52, 5, 0, 0, 0, 0, 6, 0, 0, 50],
+        [52, 5, 0, 0, 0, 0, 6, 0, 0, 50],
+        [52, 0, 6, 0, 0, 5, 0, 0, 5, 50],
+        [52, 0, 0, 0, 0, 0, 0, 0, 6, 50],
+        [52, 6, 0, 5, 0, 6, 0, 0, 0, 50],
+        [52, 0, 6, 0, 5, 0, 6, 0, 0, 50],
+        [52, 0, 5, 0, 6, 0, 5, 6, 0, 50],
+        [52, 0, 0, 5, 0, 0, 0, 0, 5, 50],
+        [52, 0, 0, 0, 6, 0, 0, 6, 0, 50],
+        [146, 19, 19, 19, 19, 19, 19, 19, 19, 147]
+    ];
 }
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,41 +1,20 @@
 // import { Viewport } from "./viewport_gl.js";
 import { Viewport } from "./viewport_2d.js"
-import { Game } from "./game.js";
+import { Game, Controller } from "./game.js";
 
 const viewport = new Viewport(<HTMLCanvasElement>document.getElementById('canvas'));
 const game = new Game();
-
-function launchGame() {
-    console.log("Launch Game");
-}
-
-function launchSimulation() {
-    console.log("Launch Simulation");
-}
+const controller = new Controller();
 
 function updateViewport() {
-    console.log('event triggered');
-    let map = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
-        [0, 0, 0, 9, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 4, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 4, 0, 0, 0, 0, 0],
-        [0, 0, 0, 9, 4, 5, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 23, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ];
+    let map = game.mapRect(0, 0, 10, 10);
     viewport.draw(map);
 }
 
-window.addEventListener("keydown", (event: KeyboardEvent) => updateViewport())
+game.update = () => updateViewport();
 
-// ==============
-game.updatePlayer(id=27);
-// game.runSteps()
-let map = game.mapRect(0, 0, 10, 10);
-viewport.draw(map);
-// ==============
+game.addObject({name: "chest", coordinate:{x:5, y:5}, tileID: 200});
+game.addObject({name: "key", coordinate:{x:5, y:7}, tileID: 240});
 
+updateViewport();
+game.loop();
