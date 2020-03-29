ca7ee185c465b8c362ebf0179ba214bf3039af1d
GreenMoonMoon <josue.boisvert@gmail.com>
2020-03-28 21:43:59 -0400

    change of plan, going javascript, typescript is way too annoying

--- javascript/viewport_gl2.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Viewport {
-    private width;
-    private height;
-    gl: WebGL2RenderingContext;
-    constructor(canvas: HTMLCanvasElement);
-    draw(buffers: any, program: any, attributeLocations: any): void;
-}
-export declare function loadShaderSources(vertexName: string, fragmentName: string): Promise<any>;
